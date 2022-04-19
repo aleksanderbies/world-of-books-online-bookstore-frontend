449c000ee5e977d303f9548fc1ac5d630b076289
Aleksander Bies <aleksanderbies@gmail.com>
2022-04-19 16:33:25 +0200

    cart view works

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import ProductView from '../views/ProductView.vue';
 import CategoryView from '../views/CategoryView.vue';
-
+import SearchView from '../views/SearchView.vue';
+import CartView from '../views/CartView.vue';
 
 const routes = [
   {
@@ -17,6 +18,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/search',
+    name: 'search',
+    component: SearchView
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: CartView
   },
   {
     path: '/:category_slug/:product_slug/',
